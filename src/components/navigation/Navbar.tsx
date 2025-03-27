b8208cc677630234b8971d00b4cf4d11f527f93d
kevis-mevada <kevis.mevada@gmail.com>
2025-03-26 23:25:57 -0700

    update-image

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
                     className="flex items-center space-x-2"
                   >
                     <Image
-                      src="/logo.png"
+                      src="/images/logo.png"
                       alt="FoodShare"
                       width={120}
                       height={40}
